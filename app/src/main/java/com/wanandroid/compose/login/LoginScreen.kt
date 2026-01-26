4b67bd9d14d755756df24c3fbf01d8bd78c1e157
wenjie <wenjie@thejoyrun.com>
2026-01-26 19:01:35 +0800

    收藏

--- app/src/main/java/com/wanandroid/compose/login/LoginScreen.kt
+++ app/src/main/java/com/wanandroid/compose/login/LoginScreen.kt
@@ -1,7 +1,6 @@
 package com.wanandroid.compose.login
 
 import android.widget.Toast
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -41,8 +40,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
