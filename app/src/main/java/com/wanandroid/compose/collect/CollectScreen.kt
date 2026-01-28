3a475d7179d666146e6d44f1ff6dbef05f11bccf
wenjie <wenjie@thejoyrun.com>
2026-01-28 15:41:31 +0800

    我的收藏功能完善

--- app/src/main/java/com/wanandroid/compose/collect/CollectScreen.kt
+++ app/src/main/java/com/wanandroid/compose/collect/CollectScreen.kt
@@ -1,12 +1,14 @@
 package com.wanandroid.compose.collect
 
 import android.text.Html
+import android.util.Log
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -18,12 +20,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -33,6 +37,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -40,9 +45,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,6 +60,9 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemKey
 import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter
+import coil3.compose.SubcomposeAsyncImage
+import coil3.compose.rememberAsyncImagePainter
 import com.wanandroid.compose.LocalBackStack
 import com.wanandroid.compose.R
 import com.wanandroid.compose.bean.ArticleItem
@@ -206,15 +216,47 @@ fun CollectionItem(
                 modifier = Modifier.fillMaxWidth()
             ) {
                 if (!articleItem.envelopePic.isNullOrBlank()) {
-                    AsyncImage(
-                        model = articleItem.envelopePic,
-                        contentDescription = null,
+                    SubcomposeAsyncImage(
                         modifier = Modifier
-                            .padding(end = 12.dp)
                             .width(120.dp)
-                            .height(90.dp),
-                        contentScale = ContentScale.Crop
+                            .height(90.dp)
+                            .padding(end = 12.dp),
+                        model = articleItem.envelopePic.replace("http", "https"),
+                        loading = {
+                            Box(
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator()
+                            }
+                        },
+                        error = {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                                modifier = Modifier
+                                    .background(MaterialTheme.colorScheme.tertiary),
+                                contentScale = ContentScale.Crop,
+                                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                                contentDescription = null
+                            )
+                        },
+                        success = {
+                            Image(
+                                painter = it.painter,
+                                contentScale = ContentScale.Crop,
+                                contentDescription = null
+                            )
+                        },
+                        contentDescription = null
                     )
+//                    AsyncImage(
+//                        model = articleItem.envelopePic,
+//                        contentDescription = null,
+//                        modifier = Modifier
+//                            .padding(end = 12.dp)
+//                            .width(120.dp)
+//                            .height(90.dp),
+//                        contentScale = ContentScale.Crop
+//                    )
                 }
                 Column(
                     modifier = Modifier.fillMaxWidth()
@@ -229,7 +271,7 @@ fun CollectionItem(
                     )
                     if (articleItem.desc.isNotBlank()) {
                         Text(
-                            text = Html.fromHtml(articleItem.desc , 0).toString(),
+                            text = Html.fromHtml(articleItem.desc, 0).toString(),
                             color = MaterialTheme.colorScheme.secondary,
                             style = MaterialTheme.typography.bodyMedium,
                             modifier = Modifier,
