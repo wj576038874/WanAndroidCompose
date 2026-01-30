0253573541e5475f4ee23c779b1cca054f140e74
wenjie <wenjie@thejoyrun.com>
2026-01-30 10:32:05 +0800

    积分布局优化采用约束布局

--- app/src/main/java/com/wanandroid/compose/coin/CoinScreen.kt
+++ app/src/main/java/com/wanandroid/compose/coin/CoinScreen.kt
@@ -4,22 +4,32 @@ import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemKey
 import com.wanandroid.compose.R
+import com.wanandroid.compose.bean.CoinItem
 import com.wanandroid.compose.common.LazyColumnPaging
 import com.wanandroid.compose.common.CommonToolbar
 import com.wanandroid.compose.http.RetrofitHelper
 import com.wanandroid.compose.route.RouteNavKey
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 /**
  * Created by wenjie on 2026/01/27.
@@ -56,14 +66,10 @@ fun CoinScreen(modifier: Modifier = Modifier) {
                     }
                 ) { index ->
                     lazyPagingItems[index]?.let { item ->
-                        Text(
-                            text = item.desc,
+                        CoinItem(
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .clickable {
-                                    Log.e("asd", "click ${item.desc}")
-                                }
-                                .padding(16.dp)
+                                .fillMaxWidth(),
+                            coinItem = item
                         )
                     }
                 }
@@ -71,3 +77,74 @@ fun CoinScreen(modifier: Modifier = Modifier) {
         )
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun CoinItemPreview() {
+    CoinItem(
+        coinItem = CoinItem(
+            coinCount = 10,
+            date = 1674841600000,
+            desc = "2026-01-26 11:55:23 签到 , 积分：11 + 22026-01-26 11:123",
+            id = 100,
+            reason = "测试",
+            type = 1,
+            userId = 100,
+            userName = "测试"
+        )
+    )
+}
+
+@Composable
+fun CoinItem(
+    modifier: Modifier = Modifier,
+    coinItem: CoinItem
+) {
+    val date  = SimpleDateFormat(
+        "yyyy-MM-dd HH:mm:ss", Locale.getDefault()
+    ).format(coinItem.date)
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable{}
+            .padding(16.dp)
+    ) {
+        val (tvCoinCount, tvDate, tvDesc) = createRefs()
+        Text(
+            text = coinItem.desc,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.constrainAs(tvDesc) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                bottom.linkTo(tvDate.top, 8.dp)
+                end.linkTo(tvCoinCount.start, 16.dp)
+                width = Dimension.fillToConstraints
+            }
+        )
+        Text(
+            text = date,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.tertiary,
+            modifier = Modifier.constrainAs(tvDate) {
+                top.linkTo(tvDesc.bottom)
+                start.linkTo(parent.start)
+                bottom.linkTo(parent.bottom)
+            }
+        )
+        Text(
+            text = "${coinItem.coinCount}",
+            color = MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.constrainAs(tvCoinCount) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                end.linkTo(parent.end)
+            }
+        )
+    }
+}
+
+
